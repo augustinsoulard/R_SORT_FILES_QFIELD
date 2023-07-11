e6c97fd9d992599e5b1bd12822ac0a622605c387
Augustin Soulard <42175782+augustinsoulard@users.noreply.github.com>
2023-07-11 14:15:41 +0200

    Update TRI_PROJET_BOTA.R
    v0.1.1

--- TRI_PROJET_BOTA.R
+++ TRI_PROJET_BOTA.R
@@ -4,7 +4,7 @@ library(tidyverse)
 path =  "\\\\SERVEUR/Etudes/Environnement/Venelles/1146-EE-PLUIh-Rhone-Crussol/Cartographie/BOTA/"
 
 FLORE = read.dbf(paste0(path,"Flore/FloreDocType.dbf"))
-
+FLORE$id = 1:nrow(FLORE)
 # RP = read.dbf("Flore/FloreDocType.dbf")
 # FLORERP = read.dbf("Flore/FloreDocType.dbf")
 
@@ -14,10 +14,10 @@ FILES = list.files("DCIM")
 for(i in 1:nrow(FLORE)){
   cat(FLORE$Photo1[i])
   if(FLORE$Photo1[i] %in% paste0("DCIM/",FILES)){
-    file.rename(as.character(FLORE$Photo1[i]),paste0("DCIM/",FLORE$NomComplet[i],".jpg"))
+    file.rename(as.character(FLORE$Photo1[i]),paste0("DCIM/",FLORE$NomComplet[i],FLORE$id[i],".jpg"))
   }
   if(FLORE$Photo2[i] %in% paste0("DCIM/",FILES)){
-    file.rename(as.character(FLORE$Photo2[i]),paste0("DCIM/",FLORE$NomComplet[i],"2.jpg"))
+    file.rename(as.character(FLORE$Photo2[i]),paste0("DCIM/",FLORE$NomComplet[i],FLORE$id[i],"_2.jpg"))
   }
 }
 
@@ -26,13 +26,15 @@ for(i in 1:nrow(FLORE)){
 HAB = read.dbf(paste0(path,"Habitats/Habitat_FR.dbf"))
 HAB$HABLABEL_COR = HAB$HABLABEL %>% str_remove_all("<em>") %>% str_remove_all("</em>")
 
+HAB$id = 1:nrow(HAB)
+
 for(i in 1:nrow(HAB)){
   cat(HAB$PHOTO1[i])
   if(HAB$PHOTO1[i] %in% paste0("DCIM/",FILES)){
-    file.rename(as.character(HAB$PHOTO1[i]),paste0("DCIM/",HAB$HABLABEL_COR[i],".jpg"))
+    file.rename(as.character(HAB$PHOTO1[i]),paste0("DCIM/",HAB$HABLABEL_COR[i],HAB$id[i],".jpg"))
   }
   if(HAB$PHOTO2[i] %in% paste0("DCIM/",FILES)){
-    file.rename(as.character(HAB$PHOTO2[i]),paste0("DCIM/",HAB$HABLABEL_COR[i],"2.jpg"))
+    file.rename(as.character(HAB$PHOTO2[i]),paste0("DCIM/",HAB$HABLABEL_COR[i],HAB$id[i],"_2.jpg"))
   }
 }
 
