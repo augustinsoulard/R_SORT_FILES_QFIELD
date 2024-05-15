d546b85d5f6685baf657943a19a7a058334638a1
Augustin Soulard <augustinsoulard@yahoo.fr>
2024-05-15 12:25:44 +0200

    v0.4.2

--- ENVIRONNEMENT_GLOBAL.R
+++ ENVIRONNEMENT_GLOBAL.R
@@ -4,5 +4,12 @@ if(!require("tidyverse")){install.packages("tidyverse")} ; library("tidyverse")
 # Definition du repertoire de fichiers
 WD = dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(WD)
+
+#Téléchargement des scripts de l'environnement
 url_enjeu_paca = "https://raw.githubusercontent.com/augustinsoulard/ENJEU_FLORE/main/ENJEU_PACA_AUGUSTIN_SOULARD/workflow/Creation_method_enjeu_paca.R"
 download.file(url_enjeu_paca, destfile = "Creation_method_enjeu_paca.R" , mode = "wb")
+
+
+
+#Téléchargement des données de l'environnement
+
